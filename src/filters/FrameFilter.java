9656d7c764f39ca8df12de63280e2d97c7bcdf1f
hoddi84 <hoddipsx@hotmail.co.uk>
2015-12-09 11:12:34 +0000

    FrameFilter

--- src/filters/FrameFilter.java
+++ src/filters/FrameFilter.java
@@ -2,6 +2,7 @@ package filters;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -25,16 +26,20 @@ public class FrameFilter implements WebcamImageTransformer {
         int w = image.getWidth();
         int h = image.getHeight();
 
-        BufferedImage modified = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        BufferedImage retVal = createCompatibleDestImage(image, null);//added
+        //BufferedImage modified = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 
-        Graphics2D g2 = modified.createGraphics();
+        //Graphics2D g2 = modified.createGraphics();
+        Graphics2D g2 = retVal.createGraphics();//added
         g2.drawImage(image, null, 0, 0);
         g2.drawImage(funFrame, null, 0, 0);
         g2.dispose();
 
-        modified.flush();
+        //modified.flush();
+        retVal.flush();//added
 
-        return modified;
+        //return modified;
+        return retVal;//added
     }
 
     private static final BufferedImage getImage(String image) {
@@ -43,6 +48,12 @@ public class FrameFilter implements WebcamImageTransformer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private BufferedImage createCompatibleDestImage(BufferedImage src, ColorModel dstCM) {
+        if ( dstCM == null )
+            dstCM = src.getColorModel();
+        return new BufferedImage(dstCM, dstCM.createCompatibleWritableRaster(src.getWidth(), src.getHeight()), dstCM.isAlphaPremultiplied(), null);
     }
 }
 
